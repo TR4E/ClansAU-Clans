135e6b7de2fb540dd2eb02bde090775f7eae20d0
TR4E <trae.rohleder@outlook.com>
2024-09-11 18:12:39 +1000

    Changes (Mostly Weapon Framework)

--- src/main/java/me/trae/clans/weapon/weapons/WindBlade.java
+++ src/main/java/me/trae/clans/weapon/weapons/WindBlade.java
@@ -2,12 +2,17 @@ package me.trae.clans.weapon.weapons;
 
 import me.trae.clans.Clans;
 import me.trae.clans.weapon.WeaponManager;
+import me.trae.core.utility.UtilBlock;
+import me.trae.core.utility.UtilMessage;
 import me.trae.core.weapon.data.WeaponData;
-import me.trae.core.weapon.types.Legendary;
+import me.trae.core.weapon.types.ChannelLegendary;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
-public class WindBlade extends Legendary<Clans, WeaponManager, WeaponData> {
+import java.util.Collections;
+
+public class WindBlade extends ChannelLegendary<Clans, WeaponManager, WeaponData> {
 
     public WindBlade(final WeaponManager manager) {
         super(manager, new ItemStack(Material.DIAMOND_SWORD));
@@ -21,5 +26,30 @@ public class WindBlade extends Legendary<Clans, WeaponManager, WeaponData> {
     @Override
     public String[] getDescription() {
         return new String[0];
+    }
+
+    @Override
+    public String getAbilityName() {
+        return "Wind Rider";
+    }
+
+    @Override
+    public void onActivate(final Player player) {
+        this.addUser(new WeaponData(player));
+    }
+
+    @Override
+    public boolean canActivate(final Player player) {
+        if (UtilBlock.isInLiquid(player.getLocation())) {
+            UtilMessage.simpleMessage(player, "Weapon", "You cannot use <green><var></green> while in liquid.", Collections.singletonList(this.getAbilityName()));
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onUsing(final Player player, final WeaponData data) {
+        UtilMessage.message(player, "Wind Blade", "Using...");
     }
 }
