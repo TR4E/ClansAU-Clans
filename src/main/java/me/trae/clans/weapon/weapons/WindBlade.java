28d6e9ef621a048a6f9c195fd41861ad14fcadf9
TR4E <trae.rohleder@outlook.com>
2024-09-11 14:41:51 +1000

    Changes (Mostly Weapon Framework)

--- src/main/java/me/trae/clans/weapon/weapons/WindBlade.java
+++ src/main/java/me/trae/clans/weapon/weapons/WindBlade.java
@@ -2,13 +2,24 @@ package me.trae.clans.weapon.weapons;
 
 import me.trae.clans.Clans;
 import me.trae.clans.weapon.WeaponManager;
+import me.trae.core.weapon.data.WeaponData;
 import me.trae.core.weapon.types.Legendary;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
-public class WindBlade extends Legendary<Clans, WeaponManager> {
+public class WindBlade extends Legendary<Clans, WeaponManager, WeaponData> {
 
     public WindBlade(final WeaponManager manager) {
         super(manager, new ItemStack(Material.DIAMOND_SWORD));
+    }
+
+    @Override
+    public Class<WeaponData> getClassOfData() {
+        return WeaponData.class;
+    }
+
+    @Override
+    public String[] getDescription() {
+        return new String[0];
     }
 }
