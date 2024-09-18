9df45982db80ac9a7604d4a918d3d8ac631d5f4b
TR4E <trae.rohleder@outlook.com>
2024-09-18 14:40:43 +1000

    Changes

--- src/main/java/me/trae/clans/weapon/weapons/WindBlade.java
+++ src/main/java/me/trae/clans/weapon/weapons/WindBlade.java
@@ -1,21 +1,37 @@
 package me.trae.clans.weapon.weapons;
 
+import me.trae.api.damage.events.CustomDamageEvent;
 import me.trae.clans.Clans;
 import me.trae.clans.weapon.WeaponManager;
-import me.trae.core.utility.UtilBlock;
-import me.trae.core.utility.UtilMessage;
+import me.trae.core.Core;
+import me.trae.core.client.ClientManager;
+import me.trae.core.utility.UtilJava;
+import me.trae.core.utility.UtilString;
+import me.trae.core.utility.UtilVelocity;
+import me.trae.core.utility.objects.SoundCreator;
 import me.trae.core.weapon.data.WeaponData;
 import me.trae.core.weapon.types.ChannelLegendary;
+import org.bukkit.Effect;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.Collections;
 
 public class WindBlade extends ChannelLegendary<Clans, WeaponManager, WeaponData> {
 
     public WindBlade(final WeaponManager manager) {
         super(manager, new ItemStack(Material.DIAMOND_SWORD));
+
+        this.addPrimitive("Damage", 7.0D);
+        this.addPrimitive("Knockback-Amount", 3.0D);
+
+        this.addPrimitive("Strength", 0.9D);
+        this.addPrimitive("yAdd", 0.11D);
+        this.addPrimitive("yMax", 1.0D);
+        this.addPrimitive("groundBoost", true);
     }
 
     @Override
@@ -25,7 +41,18 @@ public class WindBlade extends ChannelLegendary<Clans, WeaponManager, WeaponData
 
     @Override
     public String[] getDescription() {
-        return new String[0];
+        final String damage = this.getPrimitiveCasted(Double.class, "Damage").toString();
+        final String knockback = this.getPrimitiveCasted(Double.class, "Knockback-Amount").intValue() * 100 + "%";
+
+        return new String[]{
+                "Once owned by the God Zephyrus,",
+                "it is rumoured the Wind Blade",
+                "grants it's owner flight.",
+                "",
+                UtilString.pair("<gray>Damage", String.format("<yellow>%s", damage)),
+                UtilString.pair("<gray>Ability", String.format("<yellow>%s", this.getAbilityName())),
+                UtilString.pair("<gray>Knockback", String.format("<yellow>%s", knockback))
+        };
     }
 
     @Override
@@ -39,17 +66,65 @@ public class WindBlade extends ChannelLegendary<Clans, WeaponManager, WeaponData
     }
 
     @Override
-    public boolean canActivate(final Player player) {
-        if (UtilBlock.isInLiquid(player.getLocation())) {
-            UtilMessage.simpleMessage(player, "Weapon", "You cannot use <green><var></green> while in liquid.", Collections.singletonList(this.getAbilityName()));
-            return false;
-        }
+    public void onUsing(final Player player, final WeaponData data) {
+        player.getWorld().playEffect(player.getLocation(), Effect.STEP_SOUND, Material.SNOW_BLOCK);
 
-        return true;
+        new SoundCreator(Sound.FIZZ, 0.5F, 1.5F).play(player.getLocation());
+
+        UtilJava.call(this.getInstance(Core.class).getManagerByClass(ClientManager.class).getClientByPlayer(player), client -> {
+            double strength = this.getPrimitiveCasted(Double.class, "Strength");
+            final double yAdd = this.getPrimitiveCasted(Double.class, "yAdd");
+            final double yMax = this.getPrimitiveCasted(Double.class, "yMax");
+            final boolean groundBoost = this.getPrimitiveCasted(Boolean.class, "groundBoost");
+
+            if (client.isAdministrating()) {
+                strength += 1.0D;
+            }
+
+            UtilVelocity.velocity(player, strength, yAdd, yMax, groundBoost);
+        });
     }
 
-    @Override
-    public void onUsing(final Player player, final WeaponData data) {
-        UtilMessage.message(player, "Wind Blade", "Using...");
+    @EventHandler
+    public void onCustomDamageForEntityAttack(final CustomDamageEvent event) {
+        if (event.isCancelled()) {
+            return;
+        }
+
+        if (event.getCause() != EntityDamageEvent.DamageCause.ENTITY_ATTACK) {
+            return;
+        }
+
+        if (!(event.getDamager() instanceof Player)) {
+            return;
+        }
+
+        if (!(this.hasWeaponByPlayer(event.getDamagerByClass(Player.class)))) {
+            return;
+        }
+
+        event.setDamage(this.getPrimitiveCasted(Double.class, "Damage"));
+        event.setKnockback(this.getPrimitiveCasted(Double.class, "Knockback-Amount"));
+    }
+
+    @EventHandler(priority = EventPriority.LOW)
+    public void onCustomDamageForFall(final CustomDamageEvent event) {
+        if (event.isCancelled()) {
+            return;
+        }
+
+        if (event.getCause() != EntityDamageEvent.DamageCause.FALL) {
+            return;
+        }
+
+        if (!(event.getDamagee() instanceof Player)) {
+            return;
+        }
+
+        if (!(this.hasWeaponByPlayer(event.getDamageeByClass(Player.class)))) {
+            return;
+        }
+
+        event.setCancelled(true);
     }
 }
