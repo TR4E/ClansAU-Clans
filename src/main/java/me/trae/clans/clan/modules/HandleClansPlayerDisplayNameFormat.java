45af256f31ff87bf29762d4924ad15e04981b505
TR4E <trae.rohleder@outlook.com>
2024-11-06 22:09:06 +1100

    Changes

---
+++ src/main/java/me/trae/clans/clan/modules/HandleClansPlayerDisplayNameFormat.java
@@ -0,0 +1,23 @@
+package me.trae.clans.clan.modules;
+
+import me.trae.clans.Clans;
+import me.trae.clans.clan.ClanManager;
+import me.trae.core.framework.types.frame.SpigotListener;
+import me.trae.core.player.events.PlayerDisplayNameEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+
+public class HandleClansPlayerDisplayNameFormat extends SpigotListener<Clans, ClanManager> {
+
+    public HandleClansPlayerDisplayNameFormat(final ClanManager manager) {
+        super(manager);
+    }
+
+    @EventHandler
+    public void onPlayerDisplayName(final PlayerDisplayNameEvent event) {
+        final Player player = event.getPlayer();
+        final Player target = event.getTarget();
+
+        event.setPlayerName(this.getManager().getClanRelationByPlayer(target, player) + player.getName());
+    }
+}
