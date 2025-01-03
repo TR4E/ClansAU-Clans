432d4f9d3285e153b43b994c2ec52eb32e1467bd
TR4E <trae.rohleder@outlook.com>
2025-01-03 21:22:08 +1100

    Changes

--- src/main/java/me/trae/clans/world/modules/interaction/item/DisableDrinkingPotions.java
+++ src/main/java/me/trae/clans/world/modules/interaction/item/DisableDrinkingPotions.java
@@ -2,9 +2,12 @@ package me.trae.clans.world.modules.interaction.item;
 
 import me.trae.clans.Clans;
 import me.trae.clans.world.WorldManager;
+import me.trae.core.Core;
+import me.trae.core.client.ClientManager;
 import me.trae.core.framework.types.frame.SpigotListener;
 import me.trae.core.utility.enums.ActionType;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -28,6 +31,12 @@ public class DisableDrinkingPotions extends SpigotListener<Clans, WorldManager> 
         }
 
         if (itemStack.getType() != Material.POTION) {
+            return;
+        }
+
+        final Player player = event.getPlayer();
+
+        if (this.getInstanceByClass(Core.class).getManagerByClass(ClientManager.class).getClientByPlayer(player).isAdministrating()) {
             return;
         }
 
