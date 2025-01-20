60b375adbfa997cb55da45410bc42dae9b75897e
TR4E <trae.rohleder@outlook.com>
2025-01-20 19:08:35 +1100

    Changes

--- src/main/java/me/trae/clans/clan/modules/weapon/HandleWeaponLocation.java
+++ src/main/java/me/trae/clans/clan/modules/weapon/HandleWeaponLocation.java
@@ -1,9 +1,12 @@
 package me.trae.clans.clan.modules.weapon;
 
 import me.trae.clans.Clans;
+import me.trae.clans.clan.Clan;
 import me.trae.clans.clan.ClanManager;
+import me.trae.clans.clan.types.AdminClan;
 import me.trae.core.framework.types.frame.SpigotListener;
 import me.trae.core.weapon.events.WeaponLocationEvent;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 
@@ -19,16 +22,31 @@ public class HandleWeaponLocation extends SpigotListener<Clans, ClanManager> {
             return;
         }
 
-        if (event.getPlayer() != null) {
-            if (this.getManager().canCast(event.getPlayer())) {
-                return;
-            }
-        } else {
-            if (this.getManager().canCast(event.getLocation())) {
-                return;
-            }
+        if (!(this.canCancel(event))) {
+            return;
         }
 
         event.setCancelled(true);
+    }
+
+    private boolean canCancel(final WeaponLocationEvent event) {
+        if (event.getObjectDataSingleton().equals("Block Effect") && event.getWeapon().getSlicedName().equals("ExtinguishingPotion") && event.getLocation().getBlock().getType() == Material.FIRE) {
+            final Clan territoryClan = this.getManager().getClanByLocation(event.getLocation());
+            if (territoryClan instanceof AdminClan) {
+                return true;
+            }
+        }
+
+        if (event.getPlayer() != null) {
+            if (this.getManager().canCast(event.getPlayer())) {
+                return false;
+            }
+        } else {
+            if (this.getManager().canCast(event.getLocation())) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
